dca07867e49b880fd23e09238a92d4c4a7a5c4ff
Steven Romriell <romriellsteven@gmail.com>
2019-07-15 16:39:35 -0600

    latest version. works with average setting

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 ## app.R ##
 library(shiny)
 library(shinydashboard)
+library(shinyWidgets)
+library(shinydashboardPlus)
 library(markdown)
 library(readr)
 library(ggplot2)
@@ -10,7 +12,9 @@ library(DT)
 library(tools)
 selectedrowindex = 0
 #Read in main data table from your local directory
-master1 <- read.csv("https://www.dropbox.com/s/fgty42qwpkzudwz/master1.txt?dl=1")
+#master1 <- read.csv("https://www.dropbox.com/s/fgty42qwpkzudwz/master1.txt?dl=1")
+################## new way to read in comma delineated file on locate machine.
+master1 <- read.csv("master1.csv", stringsAsFactors = F)
 #Read cip data table and order alphabetically
 cip2 <- read_tsv("cip_code.txt")
 cip1 <- cip2[order(cip2$CIP_Category),]
@@ -35,8 +39,31 @@ deg.code <- c(1,2,3,4,5,6,7,8,13,14,17,18,19)
 years <- c(1,2,2,3,4,1,2,1,3,1,2,1,1)
 num.years <- data.frame(deg.code, years)
 
-ui <- dashboardPage(
-  dashboardHeader(title = "E.P.I.C. Planning"),
+ui <- dashboardPagePlus(
+  header = dashboardHeaderPlus(
+    title = "E.P.I.C. Planning", titleWidth = 230,
+    left_menu = tagList(
+      dropdownBlock(
+        id = "scenerio",
+        title = "Scenerios",
+        icon = "list",
+        radioButtons(
+          inputId = "scenerio_level",
+          label = "Level of Scenerio",
+          choices = c("Basic" = "basic",
+                      "Advanced" = "advanced"))),
+      
+      dropdownBlock(
+        id = "competency",
+        title = "Competency",
+        icon = "list",
+        radioButtons(
+          inputId = "competency_level",
+          label = "Competency",
+          choices = c("Low" = "low_comp", "Average" = "ave_comp", "High" = "hi_comp"),
+          selected = "ave_comp"
+        )))),
+  
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home Page", tabName = "home", icon = icon("home")),
@@ -49,7 +76,7 @@ ui <- dashboardPage(
                menuSubItem("Salary Select", tabName = "salary"),
                menuSubItem("Tuition Select", tabName = "tuition")),
       menuItem("Scenerios", tabName = "Scenerios", icon = icon("tasks"),
-               menuSubItem("Build Scenerios", tabName = "build"),
+               menuSubItem("Build Basic Scenerios", tabName = "buildbasic"),
                menuSubItem("Compare Scenerios", tabName = "compare")),
       menuItem("Tools", tabName = "tools", icon = icon("toolbox")),
       menuItem("About", tabName = "about", icon = icon("info"))
@@ -66,8 +93,8 @@ ui <- dashboardPage(
       tabItem(tabName = "tools",
               h2("This is where tools go."),
               checkboxGroupInput(inputId = "column.names", label = "Pick the columns you would like",
-                                 selected = c("school.name", "degree.name", "occ.name", "cip.name", "cip.cat",
-                                              "soc.cat"), choices = names(master1))
+                                 selected = c("school.name", "degree.name", "occ.name", "cip.name", "entry.degree",
+                                              "Experience", "InStOff", "IGrant.Avg", "X17p", "X50p"), choices = names(master1))
       ),
       tabItem(tabName = "instructions",
               h2("Go through each tab and select the items you are certain about")
@@ -77,7 +104,7 @@ ui <- dashboardPage(
               box(
                 width = 5,
                 selectInput(inputId = "pre.school.name",
-                            label = "",
+                            label = NULL,
                             choices = levels(master1$school.name),
                             multiple = TRUE)
               )
@@ -87,7 +114,7 @@ ui <- dashboardPage(
               box(
                 width = 5,
                 selectInput(inputId = "pre.degree.name",
-                            label = "",
+                            label = NULL,
                             choices = levels(master1$degree.name),
                             multiple = TRUE)
               )
@@ -99,11 +126,11 @@ ui <- dashboardPage(
                 box(
                   width = 4,
                   checkboxGroupInput(inputId = "pre.occupation1",
-                                     label = "",
+                                     label = NULL,
                                      choices = soc_group1)),
                 box(width = 4,
                     checkboxGroupInput(inputId = "pre.occupation2",
-                                       label = "",
+                                       label = NULL,
                                        choices = soc_group2))
               )
       ),
@@ -112,22 +139,22 @@ ui <- dashboardPage(
               fluidRow(
                 box(width = 3,
                     checkboxGroupInput(inputId = "survey.Cip_Category1",
-                                       label = "Categories",
+                                       label = NULL,
                                        choices = cip_group1)
                 ),
                 box(width = 3,
                     checkboxGroupInput(inputId = "survey.Cip_Category2",
-                                       label = "",
+                                       label = NULL,
                                        choices = cip_group2)
                 ),
                 box(width = 3,
                     checkboxGroupInput(inputId = "survey.Cip_Category3",
-                                       label = "",
+                                       label = NULL,
                                        choices = cip_group3)
                 ),
                 box(width = 3,
                     checkboxGroupInput(inputId = "survey.Cip_Category4",
-                                       label = "",
+                                       label = NULL,
                                        choices = cip_group4)
                 )
               )
@@ -136,7 +163,7 @@ ui <- dashboardPage(
               h3("How much income would you like to make in 10 to 15 years?"),
               box(
                 sliderInput(inputId = "pre.income",
-                            label = "",
+                            label = NULL,
                             value = min(sort(unique(master1$X10p))),
                             min = min(sort(unique(master1$X10p))),
                             max = max(sort(unique(master1$X10p))))
@@ -146,13 +173,13 @@ ui <- dashboardPage(
               h3("How much tuition would you like to pay per years?"),
               box(
                 sliderInput(inputId = "pre.tuition",
-                            label = "",
+                            label = NULL,
                             value = max(sort(unique(master1$InStOff))),
                             min = min(sort(unique(master1$InStOff))),
                             max = max(sort(unique(master1$InStOff))))
               )
       ),
-      tabItem(tabName = "build",
+      tabItem(tabName = "buildbasic",
               fluidRow(
                 box(width = 3,
                     
@@ -207,12 +234,7 @@ ui <- dashboardPage(
       
       tabItem(tabName = "compare",
               fluidRow(
-                box(width = 2,
-                    numericInput(inputId = "age.begin", label = "Age to begin", min = 17, max = 69, value =  17)
-                ),
-                box(width = 2,
-                    numericInput(inputId = "career.length", label = "Length of career", min = 1, max = 53, value = 20)
-                )
+                uiOutput(outputId = "tenure.select")
               ),
               fluidRow(
                 uiOutput(outputId = "row.choice.table1"),
@@ -359,10 +381,31 @@ server <- function(input, output, session) {
   })
   #Table prep with filters and Column choices for second table
   new_var <- reactive({
-    table_var()[input$nvs.choice.table_rows_selected,] %>% select(occ.name, school.name, degree.name, X17p, InStOff, degree.code)
+    table_var()[input$nvs.choice.table_rows_selected,]
+    # %>% select(occ.name, school.name, degree.name, X17p, InStOff, degree.code)
     #    table_var() %>% select(input$column.names)
   })
-  
+  observe({
+    req(input$scenerio_level == "basic")
+    output$tenure.select <- renderUI({
+      fluidRow(
+        box(width = 2,
+            numericInput(inputId = "age.begin", label = "Age to begin", min = 17, max = 69, value =  17)
+        ),
+        box(width = 2,
+            numericInput(inputId = "career.length", label = "Length of career", min = 1, max = 53, value = 20)
+        ))  
+    })
+  })
+  observe({
+    req(input$scenerio_level == "advanced")
+    output$tenure.select <- renderUI({
+      fluidRow(
+        box(width = 2,
+            numericInput(inputId = "tenure.length", label = "Length of Tenure", min = 0, max = 51, value = 20))
+      )
+    })
+  })
   #choice Tables after choosing rows  
   observe ({
     req(input$age.begin, input$career.length)
@@ -554,6 +597,216 @@ server <- function(input, output, session) {
           scale_colour_manual(name="Occupation", values = c("First" = "blue", "Second" = "green", "Third" = "red"),
                               labels = new_var()$school.name)  +
           xlab('AGE') +
+          ylab('Total Earnings') +
+          labs(title = "Cummulative Cash Flow") +
+          theme(plot.title = element_text(hjust = 0.5))
+      })
+      output$roi.plot <- renderPlot({
+        ggplot(roi.data, aes(x = school.n, y = roi.n)) + geom_bar(stat = "identity", width = 0.3) +
+          xlab('School') +
+          ylab('Percent') +
+          labs(title = "ROI") +
+          theme(plot.title = element_text(hjust = 0.5))
+      })
+    }
+  })
+  observe ({
+    req(input$tenure.length, input$competency_level == "ave_comp")
+    if(nrow(new_var()) > 0) {
+      output$row.choice.table1 <- renderUI({
+        box(width = 4,
+            strong("Occupation :"), 
+            new_var()$occ.name[1], br(),
+            strong("School :"), 
+            new_var()$school.name[1], br(),
+            strong("Degree :"), 
+            new_var()$degree.name[1], br(),
+            strong("Salary :"), 
+            new_var()$X17p[1], br(),
+            strong("Tuition :"), 
+            new_var()$InStOff[1])
+      })
+      dc <- new_var()$degree.code[1]
+      years <- as.numeric (filter(num.years, deg.code %in% dc) %>% select(years))
+      y <- 0
+      a <- 0
+      age <- 0
+      occf <- as.numeric(new_var()[1,] %>% select(MedOccF))
+      for(i in (1:years)) {
+        tf <- (1 + ((0.00002 * ((a) ^ 2)) - 0.0023 * (a) + 0.0603 ))
+        x <- as.double(new_var()[1,] %>% select(InStOff))
+        y <- y - x
+        age <- age + 1
+        s1 <- list(age1 = age, ten_factor1 = tf, occ_factor = new_var()$MedOccF[1], xsalary1 = x, run_total1 = y)
+        salary1 <- rbind(salary1, s1)
+      }
+      x <- as.double(new_var()[1,] %>% select(X17p))
+      a <- 0
+      
+      for(i in (0:input$tenure.length)) {
+        tf <- (1 + ((0.00002 * ((i + a)^ 2)) - 0.0023 * (i + a) + 0.0603 ))
+        x <- x * tf * occf
+        x <- round(x, 0)
+        y <- x + y
+        age <- age + 1
+        s1 <- list(age1 = age, ten_factor1 = tf, occ_factor = new_var()$MedOccF[1], xsalary1 = x, run_total1 = y)
+        salary1 <- rbind(salary1, s1)
+      }
+      totalcost1 <- new_var()$InStOff[1] * years
+      runtot1 <- (salary1$run_total[nrow(salary1)])
+      roi1 <- (runtot1 + totalcost1) / totalcost1 * 100
+      r1 <- list(school.n = paste("1",new_var()$school.name[1], "\n", new_var()$occ.name[1]), roi.n = roi1)
+      roi.data <- rbind(roi.data, r1)
+      
+      output$row.choice.wage1 <- renderDataTable({
+        DT::datatable(data = salary1, options = list(pageLength = 10, searching = FALSE, ordering = FALSE),
+                      selection = "none")
+      })
+      output$cummulative.plot <- renderPlot({
+        ggplot() + geom_line(data = salary1, aes(x = age1 ,y = run_total1/1000, colour="First"),
+                             show.legend = TRUE) +
+          scale_colour_manual(name="Occupation", values = c("First" = "blue", "Second" = "green", "Third" = "red"),
+                              labels = new_var()$school.name) +
+          xlab('Tenure') +
+          ylab('Total Earnings') +
+          labs(title = "Cummulative Cash Flow") +
+          theme(plot.title = element_text(hjust = 0.5))
+      })
+      output$roi.plot <- renderPlot({
+        ggplot(roi.data, aes(x=school.n, y = roi.n)) + geom_bar(stat = "identity", width = 0.4) +
+          xlab('School') + ylab('Percent') + labs(title = "ROI") +
+          theme(plot.title = element_text(hjust = 0.5))
+      })
+    }
+    if(nrow(new_var()) > 1) {
+      output$row.choice.table2 <- renderUI({
+        box(width = 4,
+            strong("Occupation :"), 
+            new_var()$occ.name[2], br(),
+            strong("School :"), 
+            new_var()$school.name[2], br(),
+            strong("Degree :"), 
+            new_var()$degree.name[2], br(),
+            strong("Salary :"), 
+            new_var()$X17p[2], br(),
+            strong("Tuition :"), 
+            new_var()$InStOff[2])
+      })
+      dc <- new_var()$degree.code[2]
+      years <- as.numeric (filter(num.years, deg.code %in% dc) %>% select(years))
+      y <- 0
+      a <- 0
+      occf <- as.numeric(new_var()[2,] %>% select(MedOccF))
+      age <- 0
+      for(i in (1:years)) {
+        tf <- (1 + ((0.00002 * ((a) ^ 2)) - 0.0023 * (a) + 0.0603 ))
+        x <- as.double(new_var()[2,] %>% select(InStOff))
+        y <- y - x
+        age <- age + 1
+        s2 <- list(age1 = age, ten_factor1 = tf, occ_factor = new_var()$MedOccF[2], xsalary1 = x, run_total1 = y)
+        salary2 <- rbind(salary2, s2)
+      }
+      x <- as.double(new_var()[2,] %>% select(X17p))
+      a <- 0
+      for(i in (0:input$tenure.length)) {
+        tf <- (1 + ((0.00002 * ((i + a)^ 2)) - 0.0023 * (i + a) + 0.0603 ))
+        x <- x * tf * occf
+        x <- round(x, 0)
+        y <- x + y
+        age <- age + 1
+        s2 <- list(age1 = age, ten_factor1 = tf, occ_factor = new_var()$MedOccF[2], xsalary1 = x, run_total1 = y)
+        salary2 <- rbind(salary2, s2)
+      }
+      totalcost1 <- new_var()$InStOff[2] * years
+      runtot1 <- (salary2$run_total[nrow(salary2)])
+      roi2 <- (runtot1 + totalcost1) / totalcost1 * 100
+      r2 <- list(school.n = paste("2", new_var()$school.name[2], "\n", new_var()$occ.name[2]), roi.n = roi2)
+      roi.data <- rbind(roi.data, data.frame(as.list(r2)))
+      
+      output$row.choice.wage2 <- renderDataTable({
+        DT::datatable(data = salary2, options = list(pageLength = 10, searching = FALSE, ordering = FALSE),
+                      selection = "none")
+      })
+      
+      output$cummulative.plot <- renderPlot({
+        ggplot() + geom_line(data = salary1, aes(x = age1 ,y = run_total1/1000, colour = "First"),
+                             show.legend = TRUE) +
+          geom_line(data = salary2, aes(x = age1, y = run_total1/1000, colour = "Second"), 
+                    show.legend = TRUE) +
+          scale_colour_manual(name="Occupation", values = c("First" = "blue", "Second" = "green", "Third" = "red"),
+                              labels = new_var()$school.name)  +
+          xlab('Tenure') +
+          ylab('Total Earnings') +
+          labs(title = "Cummulative Cash Flow") +
+          theme(plot.title = element_text(hjust = 0.5))
+      })
+      output$roi.plot <- renderPlot({
+        ggplot(roi.data, aes(x = school.n, y = roi.n)) + geom_bar(stat = "identity", width = 0.3) +
+          xlab('School') +
+          ylab('Percent') +
+          labs(title = "ROI") +
+          theme(plot.title = element_text(hjust = 0.5))
+      })
+    }
+    if(nrow(new_var()) > 2) {
+      output$row.choice.table3 <- renderUI({
+        box(width = 4,
+            strong("Occupation :"), 
+            new_var()$occ.name[3], br(),
+            strong("School :"), 
+            new_var()$school.name[3], br(),
+            strong("Degree :"), 
+            new_var()$degree.name[3], br(),
+            strong("Salary :"), 
+            new_var()$X17p[3], br(),
+            strong("Tuition :"), 
+            new_var()$InStOff[3])
+      })
+      dc <- new_var()$degree.code[3]
+      years <- as.numeric (filter(num.years, deg.code %in% dc) %>% select(years))
+      y <- 0
+      a <- 0
+      occf <- as.numeric(new_var()[3,] %>% select(MedOccF))
+      age <- 0
+      for(i in (1:years)) {
+        tf <- (1 + ((0.00002 * ((a) ^ 2)) - 0.0023 * (a) + 0.0603 ))
+        x <- as.double(new_var()[3,] %>% select(InStOff))
+        y <- y - x
+        age <- age + 1
+        s3 <- list(age1 = age, ten_factor1 = tf, occ_factor = new_var()$MedOccF[3], xsalary1 = x, run_total1 = y)
+        salary3 <- rbind(salary3, s3)
+      }
+      x <- as.double(new_var()[3,] %>% select(X17p))
+      a <- 0
+      for(i in (0:input$tenure.length)) {
+        tf <- (1 + ((0.00002 * ((i + a)^ 2)) - 0.0023 * (i + a) + 0.0603 ))
+        x <- x * tf * occf
+        x <- round(x, 0)
+        y <- x + y
+        age <- age + 1
+        s3 <- list(age1 = age, ten_factor1 = tf, occ_factor = new_var()$MedOccF[3], xsalary1 = x, run_total1 = y)
+        salary3 <- rbind(salary3, s3)
+      }
+      totalcost1 <- new_var()$InStOff[3] * years
+      runtot1 <- (salary3$run_total[nrow(salary3)])
+      roi3 <- (runtot1 + totalcost1) / totalcost1 * 100
+      r3 <- list(school.n = paste("3", new_var()$school.name[3], "\n", new_var()$occ.name[3]), roi.n = roi3)
+      roi.data <- rbind(roi.data, data.frame(as.list(r3)))
+      
+      output$row.choice.wage3 <- renderDataTable({
+        DT::datatable(data = salary3, options = list(pageLength = 10, searching = FALSE, ordering = FALSE),
+                      selection = "none")
+      })
+      output$cummulative.plot <- renderPlot({
+        ggplot() + geom_line(data = salary1, aes(x = age1 ,y = run_total1/1000, colour = "First"),
+                             show.legend = TRUE) +
+          geom_line(data = salary2, aes(x = age1, y = run_total1/1000, colour = "Second"),
+                    show.legend = TRUE) +
+          geom_line(data = salary3, aes(x = age1, y = run_total1/1000, colour = "Third"),
+                    show.legend = TRUE) +
+          scale_colour_manual(name="Occupation", values = c("First" = "blue", "Second" = "green", "Third" = "red"),
+                              labels = new_var()$school.name)  +
+          xlab('Tenure') +
           ylab('Total Earnings') +
           labs(title = "Cummulative Cash Flow") +
           theme(plot.title = element_text(hjust = 0.5))
