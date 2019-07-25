1b89c04fe94d8637d139aa78b1a173290610fe40
John Chapman <684704+chapmjs@users.noreply.github.com>
2019-07-24 18:19:18 -0600

    removed remove command

--- app.R
+++ app.R
@@ -12,6 +12,7 @@ library(DT)
 library(tools)
 selectedrowindex = 0
 #Read in main data table from your local directory
+#rm(master1)
 master1 <- read.csv("https://www.dropbox.com/s/fgty42qwpkzudwz/master1.txt?dl=1", stringsAsFactors = F)
 ################## new way to read in comma delineated file on locate machine.
 #master1 <- read.csv("master1.csv", stringsAsFactors = F)
